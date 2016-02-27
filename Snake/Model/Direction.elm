d18c51a7b24ebc9de3ea20adcc0c5c97fc96ee60
marlonliu <liuwenxuan@uchicago.edu>
2016-02-26 23:21:09 -0600

    AI that tends to walk away from where it came from

---
+++ Snake/Model/Direction.elm
@@ -0,0 +1,17 @@
+module Snake.Model.Direction where
+
+
+type Direction = Up | Down | Left | Right
+
+
+insertTurn : Direction -> List Direction -> List Direction
+insertTurn t ts =
+    case ts of
+        [] ->
+            [t]
+        h :: [] ->
+            [h, t]
+        old :: new :: [] ->
+            [new, t]
+        _ ->
+            Debug.crash "multiple directions in lastTurns"
